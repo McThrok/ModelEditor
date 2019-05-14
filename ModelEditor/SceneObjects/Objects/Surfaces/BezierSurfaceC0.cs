8aea888d4c30ab69997df28ec14080ad97262715
McThrok <wojtek.trze@gmail.com>
2019-05-14 13:56:17 +0100

    add check funciton

--- ModelEditor/SceneObjects/Objects/Surfaces/BezierSurfaceC0.cs
+++ ModelEditor/SceneObjects/Objects/Surfaces/BezierSurfaceC0.cs
@@ -13,7 +13,7 @@ namespace ModelEditor
     public class BezierSurfaceC0 : BezierSurfaceBaseC0, IRenderableObj
     {
         private static int _count = 0;
-        public Dictionary<Vertex, Vertex> Linked { get; private set; } = new Dictionary<Vertex, Vertex>();
+        public Dictionary<Vertex, Vertex> LinkedVertices { get; private set; } = new Dictionary<Vertex, Vertex>();
 
         public BezierSurfaceC0(RayCaster rayCaster) : base(rayCaster)
         {
@@ -165,10 +165,12 @@ namespace ModelEditor
 
             return Vector2Int.Empty;
         }
-        public bool IsOnEdge(Vertex vert)
+        public bool IsOnCorner(Vertex vert)
         {
             var idx = GetIndices(vert);
-            return idx.X % 3 == 0 && idx.Y % 3 == 0;
+            var xResult = idx.X == 0 || idx.X == HeightVertexCount - 1;
+            var yResult = idx.Y == 0 || idx.Y == WidthVertexCount - 1;
+            return xResult && yResult;
         }
 
         public static float GetMatrixDiff(Matrix4x4 a, Matrix4x4 b)
@@ -189,14 +191,14 @@ namespace ModelEditor
             if (surfA.Id == surfB.Id)
                 return;
 
-            if (surfA.Linked.Keys.Any(k => k.Id == a.Id) || surfB.Linked.Keys.Any(k => k.Id == b.Id))
+            if (surfA.LinkedVertices.Keys.Any(k => k.Id == a.Id) || surfB.LinkedVertices.Keys.Any(k => k.Id == b.Id))
                 return;
 
-            if (surfA.IsOnEdge(a) && surfB.IsOnEdge(b))
+            if (surfA.IsOnCorner(a) && surfB.IsOnCorner(b))
                 return;
 
-            surfA.Linked.Add(a, b);
-            surfB.Linked.Add(b, a);
+            surfA.LinkedVertices.Add(a, b);
+            surfB.LinkedVertices.Add(b, a);
 
             var eps = 0.000001;
 
@@ -219,6 +221,63 @@ namespace ModelEditor
             var matA = a.GlobalMatrix;
             matA.Translation = pos;
             a.GlobalMatrix = matA;
+        }
+
+        public static List<Vertex> CheckGregory(Vertex a, Vertex b, Vertex c)
+        {
+            if (!(a.Parent is BezierSurfaceC0 surfA) || !(b.Parent is BezierSurfaceC0 surfB) || !(c.Parent is BezierSurfaceC0 surfC))
+                return null;
+
+            if (surfA.Id == surfB.Id || surfA.Id == surfC.Id | surfB.Id == surfC.Id)
+                return null;
+
+
+            var linkA = surfA.LinkedVertices.ToList();
+            var linkB = surfB.LinkedVertices.ToList();
+            var linkC = surfC.LinkedVertices.ToList();
+
+            for (int i = 0; i < linkA.Count; i++)
+            {
+                var vert1 = linkA[i];
+                var vert1idxA = surfA.GetIndices(vert1.Key);
+                var vert1idxB = surfB.GetIndices(vert1.Value);
+                if (vert1idxB == Vector2Int.Empty)
+                    continue;
+
+                for (int j = 0; j < surfA.LinkedVertices.Count; j++)
+                {
+                    if (i == j)
+                        continue;
+
+                    var vert2 = linkA[j];
+                    var vert2idxA = surfA.GetIndices(vert2.Key);
+                    var vert2idxC = surfC.GetIndices(vert2.Value);
+                    if (vert2idxC == Vector2Int.Empty)
+                        continue;
+
+                    if (vert1idxA.X != vert2idxA.X && vert1idxA.Y != vert2idxA.Y)
+                        continue;
+
+                    for (int k = 0; k < linkB.Count; k++)
+                    {
+                        var vert3 = linkB[k];
+                        var vert3idxB = surfB.GetIndices(vert3.Key);
+                        var vert3idxC = surfC.GetIndices(vert3.Value);
+                        if (vert3idxC == Vector2Int.Empty)
+                            continue;
+
+                        if (vert1idxB.X != vert3idxB.X && vert1idxB.Y != vert3idxB.Y)
+                            continue;
+
+                        if (vert2idxC.X != vert3idxC.X && vert2idxC.Y != vert3idxC.Y)
+                            continue;
+
+                        return new List<Vertex>() { vert1.Key, vert2.Key, vert3.Key}
+                    }
+                }
+            }
+
+            return null;
         }
     }
 }
