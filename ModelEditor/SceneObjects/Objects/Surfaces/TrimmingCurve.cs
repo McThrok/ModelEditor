d965fae55b8bf697f7842f213498b55d611ae1f0
McThrok <wojtek.trze@gmail.com>
2019-06-01 17:37:13 +0100

    add TrimmingCurve class

---
+++ ModelEditor/SceneObjects/Objects/Surfaces/TrimmingCurve.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using System.Numerics;
+
+namespace ModelEditor
+{
+    public class TrimmingCurve : SceneObject, IRenderableObj
+    {
+        private static int _count = 0;
+
+        public TrimmingCurve()
+        {
+            Name = nameof(TrimmingCurve) + " " + _count++.ToString();
+        }
+
+        public ObjRenderData GetRenderData()
+        {
+            return new ObjRenderData();
+        }
+    }
+}
