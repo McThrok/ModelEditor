4a8e7da0582525329d64969061dfc8b30ef3ac2d
McThrok <wojtek.trze@gmail.com>
2019-06-01 18:06:40 +0100

    add FindTrimmingCurve

--- ModelEditor/SceneObjects/Objects/Surfaces/TrimmingCurve.cs
+++ ModelEditor/SceneObjects/Objects/Surfaces/TrimmingCurve.cs
@@ -9,6 +9,12 @@ using System.Numerics;
 
 namespace ModelEditor
 {
+    public interface TrimmingSurface
+    {
+        float Width { get; }
+        float Height { get; }
+        Guid Id { get; }
+    }
     public class TrimmingCurve : SceneObject, IRenderableObj
     {
         private static int _count = 0;
@@ -21,6 +27,49 @@ namespace ModelEditor
         public ObjRenderData GetRenderData()
         {
             return new ObjRenderData();
+        }
+
+        public static TrimmingCurve FindTrimmingCurve(List<TrimmingSurface> objs, float trimPrecision, Vector3 cursorPos)
+        {
+            float bestLen = 1000;
+            Vector2 p0 = Vector2.Zero;
+            Vector2 p1 = Vector2.Zero;
+
+            int divCount = 10;
+            bool sameObjects = objs[0].Id == objs[1].Id;
+            float eps = 0.5f;
+
+            for (int i = 0; i < divCount; i++)
+                for (int j = 0; j < divCount; j++)
+                    for (int k = 0; k < divCount; k++)
+                        for (int m = 0; m < divCount; m++)
+                        {
+                            float ii = 1f * i / (divCount - 1);
+                            float jj = 1f * j / (divCount - 1);
+                            float kk = 1f * k / (divCount - 1);
+                            float mm = 1f * m / (divCount - 1);
+
+                            var ev1 = evaluate(objs[0], jj, ii);
+                            var ev2 = evaluate(objs[1], mm, kk);
+                            var _lenght = Vector3.Distance(ev1, cursorPos) + Vector3.Distance(ev2, cursorPos);
+                            if (_lenght < bestLen && (!sameObjects || (eps < Math.Abs(ii - kk) && eps < Math.Abs(jj - mm))))
+                            {
+                                p0 = new Vector2(jj, ii);
+                                p0 = new Vector2(mm, kk);
+                                bestLen = _lenght;
+                            }
+                        }
+            return countGradientMethod(objs[0], objs[1], p1,p2);
+        }
+
+        private static TrimmingCurve countGradientMethod(TrimmingSurface trimmingSurface1, TrimmingSurface trimmingSurface2, Vector2 p1, object p2)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static Vector3 evaluate(TrimmingSurface trimmingSurface, float jj, float ii)
+        {
+            throw new NotImplementedException();
         }
     }
 }
