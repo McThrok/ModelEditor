d3ca3d902dd843e9afd3dd01bfc24132307ba913
McThrok <wojtek.trze@gmail.com>
2019-06-01 18:48:46 +0100

    add CountGradientMethod

--- ModelEditor/SceneObjects/Objects/Surfaces/TrimmingCurve.cs
+++ ModelEditor/SceneObjects/Objects/Surfaces/TrimmingCurve.cs
@@ -14,6 +14,7 @@ namespace ModelEditor
         float Width { get; }
         float Height { get; }
         Guid Id { get; }
+        Vector3 Evaluate(float h, float w);
     }
     public class TrimmingCurve : SceneObject, IRenderableObj
     {
@@ -49,25 +50,73 @@ namespace ModelEditor
                             float kk = 1f * k / (divCount - 1);
                             float mm = 1f * m / (divCount - 1);
 
-                            var ev1 = evaluate(objs[0], jj, ii);
-                            var ev2 = evaluate(objs[1], mm, kk);
-                            var _lenght = Vector3.Distance(ev1, cursorPos) + Vector3.Distance(ev2, cursorPos);
-                            if (_lenght < bestLen && (!sameObjects || (eps < Math.Abs(ii - kk) && eps < Math.Abs(jj - mm))))
+                            var ev1 = objs[0].Evaluate(jj, ii);
+                            var ev2 = objs[1].Evaluate(mm, kk);
+                            var len = Vector3.Distance(ev1, cursorPos) + Vector3.Distance(ev2, cursorPos);
+                            if (len < bestLen && (!sameObjects || (eps < Math.Abs(ii - kk) && eps < Math.Abs(jj - mm))))
                             {
                                 p0 = new Vector2(jj, ii);
-                                p0 = new Vector2(mm, kk);
-                                bestLen = _lenght;
+                                p1 = new Vector2(mm, kk);
+                                bestLen = len;
                             }
                         }
-            return countGradientMethod(objs[0], objs[1], p1,p2);
+
+            return CountGradientMethod(objs[0], objs[1], p0, p1);
         }
 
-        private static TrimmingCurve countGradientMethod(TrimmingSurface trimmingSurface1, TrimmingSurface trimmingSurface2, Vector2 p1, object p2)
+        private static float intersectionEpsilon = 0.001f;
+        private static int intersectionStep = 3;
+
+        private static TrimmingCurve CountGradientMethod(TrimmingSurface obj0, TrimmingSurface obj1, Vector2 value0, Vector2 value1)
+        {
+            var u = new Vector2(value0.X, value1.X);
+            var v = new Vector2(value0.Y, value1.Y);
+            var p0 = obj0.Evaluate(value0.X, value0.Y);
+            var p1 = obj1.Evaluate(value1.X, value1.Y);
+
+            Vector2 point0 = Vector2.Zero;
+            Vector2 point1 = Vector2.Zero;
+            var i = 0;
+            while (Vector3.Distance(p1, p0) > intersectionEpsilon)
+            {
+                i++;
+                if (i > 1000)
+                    return null;
+
+                point0 = new Vector2(u.X, v.X);
+                point1 = new Vector2(u.Y, v.Y);
+
+                Vector4 betterPoint;
+                try
+                {
+                    betterPoint = GetGradient(obj0, obj1, point0, point1);
+                }
+                catch (Exception e)
+                {
+                    //alert("There is no intersection. Try to put cursor in other place." + e);
+                    return null;
+                }
+
+                betterPoint *= intersectionStep;
+                p0 = obj0.Evaluate(u.X, v.X);
+                p1 = obj1.Evaluate(u.Y, v.Y);
+                Vector2 uPrev = u;
+                Vector2 vPrev = v;
+
+                u = new Vector2(uPrev.X - betterPoint.X, uPrev.Y - betterPoint.Y);
+                v = new Vector2(vPrev.X - betterPoint.Y, vPrev.Y - betterPoint.Z);
+
+            }
+       
+            return goGoNewton(obj0, obj1, u, v);
+        }
+
+        private static Vector4 GetGradient(TrimmingSurface obj0, TrimmingSurface obj1, Vector2 point0, Vector2 point1)
         {
             throw new NotImplementedException();
         }
 
-        private static Vector3 evaluate(TrimmingSurface trimmingSurface, float jj, float ii)
+        private static TrimmingCurve goGoNewton(TrimmingSurface obj0, TrimmingSurface obj1, Vector2 u, Vector2 v)
         {
             throw new NotImplementedException();
         }
